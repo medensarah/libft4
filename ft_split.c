d4e7c2918e29efa8447e2377e94d63dca47e50bd
Sarah Medeneche <smedenec@f2r5s1.clusters.42paris.fr>
2025-05-20 22:28:57 +0200

    split is done

--- ft_split.c
+++ ft_split.c
@@ -6,21 +6,51 @@
 /*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 13:35:53 by smedenec          #+#    #+#             */
-/*   Updated: 2025/05/20 18:23:44 by smedenec         ###   ########.fr       */
+/*   Updated: 2025/05/20 22:25:42 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+int	build_word(char *array, char *s, int len_word, int i)
 {
-	int	i;
+	int	index_char;
 
-	i = 0;
-	while (str[i])
-		i++;
+	index_char = 0;
+	while (len_word--)
+	{
+		array[index_char++] = s[i++];
+	}
+	array[index_char] = '\0';
 	return (i);
+}
+
+int	len_string(char *s, char c, int i)
+{
+	int	len_word;
+
+	len_word = 0;
+	while ((s[i] != c) && s[i])
+	{
+		i++;
+		len_word++;
+	}
+	return (len_word);
+}
+
+int	malloc_fail(char **array, int index_word)
+{
+	if (array[index_word] == NULL)
+	{
+		while (index_word >= 0)
+		{
+			free(array[index_word]);
+			index_word--;
+		}
+		return (1);
+	}
+	return (0);
 }
 
 int	ft_count_words(char *s, char c)
@@ -48,77 +78,53 @@ int	ft_count_words(char *s, char c)
 char	**ft_split(char *s, char c)
 {
 	int		i;
-	int		y;
-	int		z;
+	int		index_word;
 	int		words;
-	int		count;
 	char	**array;
 
 	i = 0;
-	count = 0;
+	index_word = 0;
 	words = ft_count_words(s, c);
 	if (words == 0)
 		return (NULL);
 	array = (char **)malloc(sizeof(char *) *(words + 1));
 	if (array == NULL)
 		return (NULL);
-	while (count < words)
+	while (index_word < words)
 	{
 		while (s[i] == c)
 			i++;
-		y = 0;
-		while ((s[i] != c) && s[i])
-		{
-			i++;
-			y++;
-		}
-		i = i - y;
-		array[count] = (char *)malloc(sizeof(char) *(y + 1));
-		if (array[count] == NULL)
-		{
-			while (count >= 0)
-			{
-				free(array[count]);
-				count--;
-			}
+		array[index_word] = malloc(sizeof(char) *(len_string(s, c, i) + 1));
+		if (malloc_fail(array, index_word))
 			return (NULL);
-		}
-		z = 0;
-		while (y--)
-		{
-			array[count][z] = s[i];
-			i++;
-			z++;
-		}
-		array[count][z] = '\0';
-		count++;
+		i = build_word(array[index_word++], s, len_string(s, c, i), i);
 	}
-	array[count] = NULL;
+	array[index_word] = NULL;
 	return (array);
 }
-int	main(void)
-{
-	char	s[] = "  f0 f2  f4 ";
-	char	c;
-	int		i;
-	char	**cpy;
+// int	main(void)
+// {
+// 	char	s[] = "AAA BBB CC DD";
+// 	char	c;
+// 	int		i;
+// 	char	**cpy;
 
-	c = ' ';
-	i = 0;
-	cpy = ft_split(s, c);
-	if (cpy)
-	{
-		while (cpy[i])
-		{
-		printf("a split = %s\n", cpy[i]);
-		free(cpy[i]);
-		i++;
-		}
-		free(cpy);
-	}
-	else
-	{
-		printf("a split = %s\n", (char *)cpy);
-	}
-	return (0);
-}
+// 	c = ' ';
+// 	i = 0;
+// 	cpy = ft_split(s, c);
+// 	if (cpy)
+// 	{
+// 		while (cpy[i])
+// 		{
+// 		printf("a split = %s\n", cpy[i]);
+// 		free(cpy[i]);
+// 		i++;
+// 		}
+// 		free(cpy);
+// 	}
+// 	else
+// 	{
+// 		printf("a split = %s\n", (char *)cpy);
+// 	}
+// 	return (0);
+// }
