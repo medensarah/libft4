79dff16ffe6fb89877f283edae1e36e75bf03496
Sarah Medeneche <smedenec@f2r9s3.clusters.42paris.fr>
2025-05-21 05:33:05 +0200

    striteri pas sure du tout

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,56 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 02:05:26 by smedenec          #+#    #+#             */
-/*   Updated: 2025/05/21 05:08:37 by smedenec         ###   ########.fr       */
+/*   Updated: 2025/05/21 05:32:23 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+void	*ft_striteri(char *s, char (*f)(unsigned int, char*))
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-// char	ft_assign(unsigned int i, char c)
-// {
-// 	return c;
-// }
-char	*ft_strmapi(char *s, char (*f)(unsigned int, char))
-{
-	char			*dest;
 	unsigned int	i;
 
 	i = 0;
-	if (!f || !s)
-		return (NULL);
-	dest = malloc(sizeof(char) *(ft_strlen(s) + 1));
-	if (dest == NULL)
-		return (NULL);
 	while (s[i])
 	{
-		dest[i] = f(i, s[i]);
+		f(i, s);
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
 }
+
 // int	main(void)
 // {
 // 	char	str[] = "Hackathon";
 
-// 	char *cpy = ft_strmapi(str, ft_assign);
-// 	printf("le int = %s\n", cpy);
+// 	printf("le int = %s\n", str);
 // 	return(0);
 // }
