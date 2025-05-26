79626b82b90cb6045e7a99de5747b55e03d355c0
Sarah Medeneche <smedenec@f1br6s11.clusters.42paris.fr>
2025-05-27 00:37:19 +0200

    strncmp done

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,12 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 16:49:19 by smedenec          #+#    #+#             */
-/*   Updated: 2025/05/23 20:30:34 by marvin           ###   ########.fr       */
+/*   Updated: 2025/05/27 00:36:31 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int	ft_strlen(const char *str)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (str[i])
+// 		i++;
+// 	return (i);
+// }
 
 #include "libft.h"
 
@@ -19,16 +32,21 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n <= 0)
 		return (0);
-	while (n-- > 0)
+	while (n--)
 	{
-		if (s1[i] && (s1[i] == s2[i]))
+		if (s1[i] && s2[i] && (s1[i] == s2[i]))
 			i++;
 		else
-			return (s1[i] - s2[i]);
+		{
+			if (((unsigned char)s1[i] - (unsigned char)s2[i]) > 0)
+				return (1);
+			else if (((unsigned char)s1[i] - (unsigned char)s2[i]) < 0)
+				return (-1);
+		}
 	}
 	return (0);
 }
 // int	main(void)
 // {
-// 	printf("return = %d\n", ft_strncmp("index", "de", 5));
+// 	printf("return = %d\n", ft_strncmp("abcdef", "abc\375xx", 6));
 // }
