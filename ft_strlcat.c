1ca495cad5b503008f3a6f889ea3819ee2b36428
Sarah Medeneche <smedenec@f1br10s4.clusters.42paris.fr>
2025-05-26 03:58:16 +0200

    long try strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 18:36:35 by smedenec          #+#    #+#             */
-/*   Updated: 2025/05/25 06:40:25 by marvin           ###   ########.fr       */
+/*   Updated: 2025/05/26 03:55:32 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <string.h>
 // #include <stdio.h>
+// #include <string.h>
 
 // int	ft_strlen(const char *str)
 // {
@@ -25,36 +25,64 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, char *src, size_t size)
-{
-	int	d;
-	int	s;
-
-	d = ft_strlen(dst);
-	s = ft_strlen(src);
-	if (s >= d)
-	{
-		s = 0;
-		while (src[s] && --size)
-		{
-			dst[d + s] = src[s];
-			s++;
-		}
-		dst[d + s] = '\0';
-		return (d + s);
-	}
-	else
-		return (d + s);
-}
-// int	main(void)
+// size_t	ft_strlcat(char *dst, char *src, size_t size)
 // {
-// 	size_t	size;
-// 	char	dst[] = "wallet";
-// 	char	src[] = "_point";
+// 	size_t	dst_len;
+// 	size_t	src_len;
+// 	size_t	e;
+// 	size_t	i;
 
-// 	size = 10;
-// 	printf("%zu\n", ft_strlcat(dst, src, size));
-// 	printf("dest = %s\n", dst);
-// 	printf("src = %s\n", src);
-// 	return (0);
+// 	i = 0;
+// 	src_len = ft_strlen(src);
+// 	dst_len = ft_strlen(dst);
+// 	if ((size > dst_len))
+// 	{
+// 		e = size - 1;
+// 		while (src[i] && e && ((i + dst_len) < e))
+// 		{
+// 			dst[dst_len + i] = src[i];
+// 			i++;
+// 			e--;
+// 		}
+// 		dst[dst_len + i] = '\0';
+// 	}
+// 	if (src_len == 0)
+// 		return (0);
+// 	if (size > src_len + dst_len || (dst_len < src_len && size > src_len))
+// 		return (src_len + dst_len);
+// 	else
+// 		return (src_len + size);
 // }
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	int	i;
+	int	d;
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	size_cpy;
+
+	size_cpy = size;
+	src_len = ft_strlen(src);
+ 	dst_len = ft_strlen(dst);
+	i = 0;
+	d = 0;
+	while (dst[i] && size_cpy > 0)
+	{
+		size_cpy--;
+		i++;
+	}
+	while (src[i + d] && size_cpy > 0)
+	{
+		dst[i + d] = src[d];
+		d++;
+		size_cpy--;
+
+	}
+	if (size_cpy > 0)
+		dst[i + d] = '\0';
+	if (size > src_len + dst_len || (dst_len < src_len && size > src_len))
+		return (src_len + dst_len);
+	else
+		return (src_len + size);
+}
