91fc52da2eccfcfa22236e323fb8b7f001ab8f25
Sarah Medeneche <smedenec@f1br8s15.clusters.42paris.fr>
2025-05-31 15:11:09 +0200

    putstr_fd and putstr_fd done

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,31 +6,31 @@
 /*   By: smedenec <smedenec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 02:05:26 by smedenec          #+#    #+#             */
-/*   Updated: 2025/05/31 14:46:14 by smedenec         ###   ########.fr       */
+/*   Updated: 2025/05/31 14:56:10 by smedenec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_striteri(char *s, void (*f)(unsigned int, char*))
-{
-	char			*dest;
-	unsigned int	i;
+// void	*ft_striteri(char *s, void (*f)(unsigned int, char*))
+// {
+// 	char			*dest;
+// 	unsigned int	i;
 
-	i = 0;
-	if (!f || !s)
-		return (NULL);
-	dest = malloc(sizeof(char) *(ft_strlen(s) + 1));
-	if (dest == NULL)
-		return (NULL);
-	while (s[i])
-	{
-		dest[i] = (char)f(i, s[i]);
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
+// 	i = 0;
+// 	if (!f || !s)
+// 		return (NULL);
+// 	dest = malloc(sizeof(char) *(ft_strlen(s) + 1));
+// 	if (dest == NULL)
+// 		return (NULL);
+// 	while (s[i])
+// 	{
+// 		dest[i] = (char)f(i, s[i]);
+// 		i++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
 
 // int	main(void)
 // {
